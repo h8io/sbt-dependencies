31cdaaf69ee917438d1b9384c723a3b1d47a900f
Eshu <tjano.xibalba@gmail.com>
2025-09-12 00:10:18 +0900

    Scoverage summary

--- test.sh
+++ test.sh
@@ -1,1 +1,13 @@
-sbt clean scalafmtSbtCheck scalafmtCheckAll +package
+#!/bin/bash
+
+set -euxo pipefail
+
+if [[ -n "${CI:-}" ]]; then
+  COVERAGE_AGGREGATE=
+  COVERAGE_SUMMARY=+coverageSummary
+else
+  COVERAGE_AGGREGATE=+coverageAggregate
+  COVERAGE_SUMMARY=
+fi
+
+sbt clean scalafmtSbtCheck scalafmtCheckAll +coverage +test $COVERAGE_AGGREGATE $COVERAGE_SUMMARY
